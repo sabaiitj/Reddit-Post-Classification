bda7b2aa45cf912d5a8053611901d89946dd834e
Saba Suhail <sabasuhail1112@gmail.com>
2022-05-28 01:48:35 -0700

    Day1

--- reddit_scraper.py
+++ reddit_scraper.py
@@ -9,53 +9,62 @@ reddit = praw.Reddit(client_id='89t0Tjzp4YkOenLbtGjJpw',
                      user_agent='Testing_API')
     
 
-filename = 'reddit_hot_posts.csv'
-LIMIT = 5000
+filename = 'reddit_hot_posts1.csv'
+LIMIT = 1000
 
-posts = {}
-with open(filename) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for post in csv_reader:
-        posts[post[0]] = post
+def load_posts(from_file):
+    posts = {}
+    with open(from_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for post in csv_reader:
+            posts[post[0]] = post
 
-if "post_id" in posts:        
-    del posts["post_id"]
+        if "post_id" in posts:        
+            del posts["post_id"]
+    return posts        
 
+posts = load_posts(filename)
 print(f'Loaded {len(posts)} posts from file {filename}')
 error = 0
 
-for post in reddit.subreddit('all').hot(limit=LIMIT):
-    try:
-        time_in_mins = round((datetime.utcnow() - datetime.fromtimestamp(post.created)).seconds/60)
-        row = [
-            post.id,
-            post.author.comment_karma,
-            post.author.link_karma,# 3
-            time_in_mins,
-            post.is_original_content, # 6
-            post.name, # 9
-            post.num_comments,
-            post.over_18,
-            post.score, # 12
-            post.selftext is None, 
-            post.spoiler,
-            post.stickied,
-            post.subreddit.id,
-            post.subreddit.name,
-            post.subreddit.subscribers,
-            post.title,
-            post.upvote_ratio,
-            post.url,
-        ]
-        if post.id in posts:
-            print(f'Updating {post.id}:{post.title} post.')
-        else:
-            print(f'Found new post {post.id}:{post.title}.')
-        posts[post.id] = row
-    except Exception:
-        # Skiping post with incorrect data.
-        error += 1
-#         logging.exception("Error reading row", post)
+with open(filename + '.dump', 'w') as f:
+    w = writer(f)
+    hot_posts = reddit.subreddit('all').hot(limit=LIMIT)
+    for post in hot_posts:
+        try:
+            time_in_mins = round((datetime.utcnow() - datetime.fromtimestamp(post.created)).seconds/60)
+            row = [
+                post.id,
+                post.author.comment_karma,
+                post.author.link_karma,# 3
+                time_in_mins,
+                post.is_original_content, # 6
+                post.name, # 9
+                post.num_comments,
+                post.over_18,
+                post.score, # 12
+                post.selftext is None, 
+                post.spoiler,
+                post.stickied,
+                post.subreddit.id,
+                post.subreddit.name,
+                post.subreddit.subscribers,
+                post.title,
+                post.upvote_ratio,
+                post.url,
+            ]
+            if post.id in posts:
+                print(f'Updating {post.id}:{post.title} post.')
+            else:
+                print(f'Found new post {post.id}:{post.title}.')
+            posts[post.id] = row
+            w.writerow(row)
+        except Exception:
+            # Skiping post with incorrect data.
+            error += 1
+    #         logging.exception("Error reading row", post)
+    
+        
 
 print(f'Posts with error {error}')
 
@@ -79,6 +88,7 @@ header = [
     "post_upvote_ratio",
     "post_url",
 ]
+posts = load_posts(filename + '.dump')
 with open(filename, 'w') as f:
     w = writer(f)
     w.writerow(header)
